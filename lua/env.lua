b76ec20d163fd99aa01f66d833eb68d60d339e08
Walt.Wang <twang9739@gmail.com>
2023-01-14 01:51:27 +0800

    pref(syntax): c and cpp indent

--- lua/env.lua
+++ lua/env.lua
@@ -49,7 +49,7 @@ vim.g.go_term_width = 20
 
 -- indent by filetype
 vim.api.nvim_command(
-	"au FileType html,javascript,typescript,css,less,scss,vue,proto,json set tabstop=2 softtabstop=2 shiftwidth=2"
+	"au FileType html,javascript,typescript,css,less,scss,vue,proto,json,c,cpp set tabstop=2 softtabstop=2 shiftwidth=2"
 )
 
 vim.g.translator_default_engines = { "bing", "youdao" }
