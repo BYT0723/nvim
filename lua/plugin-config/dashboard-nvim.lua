a2cb2a56616a6c712be43cfe50d5e647da45c803
BYT0723 <twang9739@gmail.com>
2023-01-26 03:29:58 +0800

    fix(dashboard): change dashboard.nvim configuration to adapt new dashboard conf

--- lua/plugin-config/dashboard-nvim.lua
+++ lua/plugin-config/dashboard-nvim.lua
@@ -1,10 +1,4 @@
-local home = os.getenv("HOME")
-local db = require("dashboard")
--- db.preview_command = "lolcat -F 0.2"
--- db.preview_file_path = home .. "/.config/nvim/neovim.cat"
--- db.preview_file_height = 8
--- db.preview_file_width = 55
-db.default_banner = {
+local custom_header = {
 	"                                                       ",
 	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
 	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
@@ -13,54 +7,78 @@ db.default_banner = {
 	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
 	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
 	"                                                       ",
+	"             [ May everyone not be alone ]             ",
+	"                                                       ",
+	"                                                       ",
 }
-db.custom_center = {
-	{
-		icon = "  ",
-		desc = "New Buffer                   ",
-		action = "DashboardNewFile",
-		shortcut = "SPC   n",
+local hyper_config = {
+	header = custom_header, -- ascii text in there
+	shortcut = {
+		{ desc = "Files", key = "SPC   e", action = "NvimTreeOpen", group = "DashboardRecentTitle" },
+		{ desc = "Search", key = "SPC f f", action = "Telescope fd", group = "DashboardRecentTitle" },
+		{ desc = "Update", key = "SPC u", action = "PackerSync", group = "DashboardRecentTitle" },
+		{ desc = "dotfiles", key = "cf", action = "edit ~/.config/nvim/init.vim", group = "DashboardRecentTitle" },
 	},
-	{
-		icon = "פּ  ",
-		desc = "Open File Tree               ",
-		action = "NvimTreeOpen",
-		shortcut = "SPC   e",
-	},
-	{
-		icon = "  ",
-		desc = "Recent Projects              ",
-		action = "Telescope projects",
-		shortcut = "SPC   p",
-	},
-	{
-		icon = "  ",
-		desc = "Search File                  ",
-		action = "Telescope fd",
-		shortcut = "SPC f f",
-	},
-	{
-		icon = "  ",
-		desc = "Search Context               ",
-		action = "Telescope live_grep",
-		shortcut = "SPC f g",
-	},
-	{
-		icon = "  ",
-		desc = "Most Recently                ",
-		action = "Telescope oldfiles",
-		shortcut = "SPC f r",
-	},
-	{
-		icon = "  ",
-		desc = "Plugin Update                ",
-		action = "PackerSync",
-		shortcut = "       ",
-	},
-	{
-		icon = "  ",
-		desc = "Configuration                ",
-		action = "edit ~/.config/nvim/init.vim",
-		shortcut = "       ",
+	packages = { enable = true }, -- show how many plugins neovim loaded
+	project = { limit = 8, action = "Telescope find_files cwd=" }, -- limit how many projects list, action when you press key or enter it will run this action.
+	mru = { limit = 10 }, -- how many files in list
+}
+
+local doom_config = {
+	header = custom_header,
+	center = {
+		{
+			icon = "  ",
+			desc = "New Buffer                ",
+			action = "enew",
+			key = "SPC   n",
+		},
+		{
+			icon = "פּ  ",
+			desc = "File Manager              ",
+			action = "NvimTreeOpen",
+			key = "SPC   e",
+		},
+		{
+			icon = "  ",
+			desc = "Recently Files            ",
+			action = "Telescope oldfiles",
+			key = "SPC f r",
+		},
+		{
+			icon = "  ",
+			desc = "Recent Projects           ",
+			action = "Telescope projects",
+			key = "SPC   p",
+		},
+		{
+			icon = "  ",
+			desc = "Search File               ",
+			action = "Telescope fd",
+			key = "SPC f f",
+		},
+		{
+			icon = "  ",
+			desc = "Search Context            ",
+			action = "Telescope live_grep",
+			key = "SPC f g",
+		},
+		{
+			icon = "  ",
+			desc = "Plugin Update             ",
+			action = "PackerSync",
+			key = "       ",
+		},
+		{
+			icon = "  ",
+			desc = "Configuration             ",
+			action = "edit ~/.config/nvim/init.vim",
+			key = "       ",
+		},
 	},
 }
+
+require("dashboard").setup({
+	theme = "doom", --  theme is doom and hyper default is hyper
+	config = doom_config,
+})
