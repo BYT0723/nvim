01a511d95f71ae28d94ad0fd1e6e6ab7ad3bb082
BYT0723 <twang9739@gmail.com>
2023-03-21 23:34:31 +0800

    pref(telescope): add extension notify

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -43,3 +43,4 @@ require("telescope").setup({
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("projects")
 require("telescope").load_extension("ui-select")
+require("telescope").load_extension("notify")
