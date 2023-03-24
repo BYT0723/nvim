8017c7533cfee549a44c4025997046c31b9e7997
BYT0723 <twang9739@gmail.com>
2023-03-25 03:55:42 +0800

    feat(mason): add PATH configuration

--- lua/plugin-config/mason.lua
+++ lua/plugin-config/mason.lua
@@ -1,18 +1,59 @@
+local required_tools = {
+  -- DAP
+  'cpptools',
+  'delve',
+  -- Linter
+  'buf',
+  'cmakelang',
+  'codespell',
+  'cpplint',
+  'golangci-lint',
+  'vint',
+  -- Formatter
+  'autopep8',
+  'cmakelang',
+  'goimports',
+  'prettierd',
+  'shfmt',
+  'sql-formatter',
+  'stylua',
+}
 require('mason').setup({
+  -- Where Mason should put its bin location in your PATH. Can be one of:
+  -- - "prepend" (default, Mason's bin location is put first in PATH)
+  -- - "append" (Mason's bin location is put at the end of PATH)
+  -- - "skip" (doesn't modify PATH)
+  ---@type '"prepend"' | '"append"' | '"skip"'
+  PATH = 'append',
+
+  -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
+  -- debugging issues with package installations.
+  log_level = vim.log.levels.INFO,
+
+  -- Limit for the maximum amount of packages to be installed at the same time. Once this limit is reached, any further
+  -- packages that are requested to be installed will be put in a queue.
+  max_concurrent_installers = 8,
+
   ui = {
     -- Whether to automatically check for new versions when opening the :Mason window.
     check_outdated_packages_on_open = true,
 
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = 'rounded', -- none / single / double / rounded / solid / shadow
+    -- Width of the window. Accepts:
+    -- - Integer greater than 1 for fixed width.
+    -- - Float in the range of 0-1 for a percentage of screen width.
+    width = 0.8,
+
+    -- Height of the window. Accepts:
+    -- - Integer greater than 1 for fixed height.
+    -- - Float in the range of 0-1 for a percentage of screen height.
+    height = 0.8,
 
     icons = {
-      -- The list icon to use for installed packages.
-      package_installed = '◍',
-      -- The list icon to use for packages that are installing, or queued for installation.
-      package_pending = '◍',
-      -- The list icon to use for packages that are not installed.
-      package_uninstalled = '◍',
+      package_installed = '✓',
+      package_pending = '➜',
+      package_uninstalled = '✗',
     },
 
     keymaps = require('keybindings').mason(),
