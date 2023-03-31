75e4030a574a59cce835993c9c56258d53ffce46
BYT0723 <twang9739@gmail.com>
2023-04-01 02:26:21 +0800

    refactor(linter): refactor linter.lua

--- lua/lsp/linter.lua
+++ lua/lsp/linter.lua
@@ -1,34 +1,24 @@
-require('lint').linters_by_ft = {
-  c = {
-    'cpplint',
-    'codespell',
-  },
-  cpp = {
-    'cpplint',
-    'codespell',
-  },
-  go = {
-    'golangcilint',
-    'codespell',
-  },
-  javascript = {
-    'codespell',
-  },
-  typescript = {
-    'codespell',
-  },
-  proto = {
-    -- "buf",
-    'codespell',
-  },
-  markdown = {
-    'codespell',
-  },
-  vimscript = {
-    'vint',
-    'codespell',
-  },
+local linters_by_ft = {
+  c = { 'cpplint' },
+  cpp = { 'cpplint' },
+  go = { 'golangcilint' },
+  vimscript = { 'vint' },
+  markdown = { 'proselint' },
+  javascript = { 'eslint_d' },
+  typescript = { 'eslint_d' },
 }
+
+local global_linters = { 'codespell' }
+
+local function addGlobals(linters)
+  local res = {}
+  for k, v in pairs(linters) do
+    res[k] = {unpack(v), unpack(global_linters)}
+  end
+  return res
+end
+
+require('lint').linters_by_ft = addGlobals(linters_by_ft)
 
 vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave', 'TextChanged' }, {
   callback = function()
