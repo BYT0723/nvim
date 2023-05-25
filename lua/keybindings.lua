80cf79928a0100403e56e9b85bea7bb0037954f4
BYT0723 <twang9739@gmail.com>
2023-05-25 23:22:14 +0800

    doc(keybind): add document

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -11,53 +11,53 @@ M.Common = {
   },
   n = {
     ['W'] = { cmd = '<cmd>w!<CR>', desc = 'Save file' },
-    ['Q'] = { cmd = '<cmd>q!<CR>', desc = 'Exit window'},
+    ['Q'] = { cmd = '<cmd>q!<CR>', desc = 'Exit window' },
     -- layout
-    ['<C-k>'] = '<cmd>resize -5<CR>',
-    ['<C-j>'] = '<cmd>resize +5<CR>',
-    ['<C-h>'] = '<cmd>vertical resize -5<CR>',
-    ['<C-l>'] = '<cmd>vertical resize +5<CR>',
+    ['<C-k>'] = { cmd = '<cmd>resize -5<CR>',          desc = 'Reduce window width' },
+    ['<C-j>'] = { cmd = '<cmd>resize +5<CR>',          desc = 'Increase window width' },
+    ['<C-h>'] = { cmd = '<cmd>vertical resize -5<CR>', desc = 'Reduce form height' },
+    ['<C-l>'] = { cmd = '<cmd>vertical resize +5<CR>', desc = 'Increase window height' },
     -- tab
-    ['tk'] = '<cmd>tabp<CR>',
-    ['tj'] = '<cmd>tabn<CR>',
-    ['tq'] = '<cmd>tabclose<CR>',
+    ['tk'] = { cmd = '<cmd>tabp<CR>',     desc = 'previous tab' },
+    ['tj'] = { cmd = '<cmd>tabn<CR>',     desc = 'next tab' },
+    ['tq'] = { cmd = '<cmd>tabclose<CR>', desc = 'close tab' },
     -- buffer
     -- ['bk'] = '<cmd>bp<CR>',
     -- ['bj'] = '<cmd>bn<CR>',
     -- ['bq'] = '<cmd>bdelete!<CR>',
     -- quickfix
-    ['ck'] = '<cmd>cp<CR>',
-    ['cj'] = '<cmd>cn<CR>',
-    ['cc'] = '<cmd>cc<CR>',
-    ['<leader>c'] = { cmd = "<cmd>lua require('util').toggle_quickfix()<CR>", desc = 'Quickfix'},
+    ['ck']        = { cmd = '<cmd>cp<CR>',                                    desc = 'previous item in quickfix' },
+    ['cj']        = { cmd = '<cmd>cn<CR>',                                    desc = 'next item in quickfix' },
+    ['cc']        = { cmd = '<cmd>cc<CR>',                                    desc = 'current item in quickfix' },
+    ['<leader>c'] = { cmd = "<cmd>lua require('util').toggle_quickfix()<CR>", desc = 'Quickfix' },
     --launcher
-    ['<leader>rf'] = { cmd = "<cmd>lua require('launcher').runFile()<CR>",             desc = "Run File" },
-    ['<leader>rp'] = { cmd = "<cmd>lua require('launcher').runProject()<CR>",          desc = "Start Project" },
-    ['<leader>ri'] = { cmd = "<cmd>lua require('launcher').getRunProjectCmd()<CR>",    desc = "Show project command" },
-    ['<leader>re'] = { cmd = "<cmd>lua require('launcher').editRunProjectCmd()<CR>",   desc = "Edit project command" },
-    ['<leader>rm'] = { cmd = "<cmd>lua require('launcher').removeRunProjectCmd()<CR>", desc = "Remove project command" },
+    ['<leader>rf'] = { cmd = "<cmd>lua require('launcher').runFile()<CR>",             desc = 'Run File' },
+    ['<leader>rp'] = { cmd = "<cmd>lua require('launcher').runProject()<CR>",          desc = 'Start Project' },
+    ['<leader>ri'] = { cmd = "<cmd>lua require('launcher').getRunProjectCmd()<CR>",    desc = 'Show project command' },
+    ['<leader>re'] = { cmd = "<cmd>lua require('launcher').editRunProjectCmd()<CR>",   desc = 'Edit project command' },
+    ['<leader>rm'] = { cmd = "<cmd>lua require('launcher').removeRunProjectCmd()<CR>", desc = 'Remove project command' },
   },
   t = {
-    ['<C-q>'] = '<C-\\><C-n>',
-    ['<C-w>j'] = '<cmd>wincmd j<CR>',
-    ['<C-w>k'] = '<cmd>wincmd k<CR>',
-    ['<C-w>h'] = '<cmd>wincmd h<CR>',
-    ['<C-w>l'] = '<cmd>wincmd l<CR>',
+    ['<C-q>']  = { cmd = '<C-\\><C-n>',       desc = 'Exit terminal mode' },
+    ['<C-w>j'] = { cmd = '<cmd>wincmd j<CR>', desc = 'Move to window below' },
+    ['<C-w>k'] = { cmd = '<cmd>wincmd k<CR>', desc = 'Move to upper window' },
+    ['<C-w>h'] = { cmd = '<cmd>wincmd h<CR>', desc = 'Move to left window' },
+    ['<C-w>l'] = { cmd = '<cmd>wincmd l<CR>', desc = 'Move to right window' },
   },
 }
 
 M.Notify = {
   n = {
-    ['<leader>un'] = { cmd = "<cmd>lua require('notify').dismiss()<CR>", desc = "Hide all notifications" },
+    ['<leader>un'] = { cmd = "<cmd>lua require('notify').dismiss()<CR>", desc = 'Hide all notifications' },
   },
 }
 
 M.PanTran = {
   n = {
-    ['<leader>tw'] = { cmd = '<cmd>Pantran mode=hover target=zh<CR>',   desc = "Translate line" },
-    ['<leader>tr'] = { cmd = '<cmd>Pantran mode=replace target=en<CR>', desc = "Translate and Replace" },
-    ['<leader>ta'] = { cmd = '<cmd>Pantran mode=append target=en<CR>',  desc = "Translate and Append"},
-    ['<leader>ti'] = { cmd = '<cmd>Pantran mode=interactive<CR>',       desc = "Translate Interactive UI" },
+    ['<leader>tw'] = { cmd = '<cmd>Pantran mode=hover target=zh<CR>',   desc = 'Translate line' },
+    ['<leader>tr'] = { cmd = '<cmd>Pantran mode=replace target=en<CR>', desc = 'Translate and Replace' },
+    ['<leader>ta'] = { cmd = '<cmd>Pantran mode=append target=en<CR>',  desc = 'Translate and Append' },
+    ['<leader>ti'] = { cmd = '<cmd>Pantran mode=interactive<CR>',       desc = 'Translate Interactive UI' },
   },
   x = {
     ['<leader>tw'] = "<cmd>'<,'>Pantran mode=hover target=zh<CR>",
@@ -69,85 +69,85 @@ M.PanTran = {
 
 M.Hop = {
   [''] = {
-    ['f'] = '<cmd>HopChar1CurrentLine<cr>',
-    ['F'] = '<cmd>HopChar2<cr>',
+    ['f'] = { cmd = '<cmd>HopChar1CurrentLine<cr>', desc = 'find single char in current line' },
+    ['F'] = { cmd = '<cmd>HopChar2<cr>',            desc = 'find two connected char' },
   },
 }
 
 M.EasyAlign = {
   n = {
-    ['ga'] = '<Plug>(EasyAlign)',
+    ['ga'] = { cmd = '<Plug>(EasyAlign)', desc = 'text align' },
   },
   x = {
-    ['ga'] = '<Plug>(EasyAlign)',
+    ['ga'] = { cmd = '<Plug>(EasyAlign)', desc = 'text align' },
   },
 }
 
 M.NvimTree = {
   n = {
-    ['<leader>e'] = { cmd = '<cmd>NvimTreeToggle<CR>', desc = "NvimTree" },
+    ['<leader>e'] = { cmd = '<cmd>NvimTreeToggle<CR>', desc = 'NvimTree' },
   },
 }
 
 M.Lspsaga = {
   n = {
-    ['<leader>v'] = '<cmd>Lspsaga outline<CR>',
+    ['<leader>v'] = { cmd = '<cmd>Lspsaga outline<CR>', desc = 'Syntax Tree' },
   },
 }
 
 M.Gitsigns = {
   n = {
-    ['<leader>gk'] = { cmd = '<cmd>Gitsigns prev_hunk<CR>',                  desc = "Jump to previous hunk" },
-    ['<leader>gj'] = { cmd = '<cmd>Gitsigns next_hunk<CR>',                  desc = "Jump to next hunk" },
-    ['<leader>gb'] = { cmd = '<cmd>Gitsigns blame_line<CR>',                 desc = "Show Blamer" },
-    ['<leader>gv'] = { cmd = '<cmd>Gitsigns preview_hunk<CR>',               desc = "Preview hunk" },
-    ['<leader>gr'] = { cmd = '<cmd>Gitsigns reset_hunk<CR>',                 desc = "Reset hunk" },
-    ['<leader>gR'] = { cmd = '<cmd>Gitsigns reset_buffer<CR>',               desc = "Reset all hunk" },
-    ['<leader>gs'] = { cmd = '<cmd>Gitsigns stage_hunk<CR>',                 desc = "Stage hunk" },
-    ['<leader>gu'] = { cmd = '<cmd>Gitsigns undo_stage_hunk<CR>',            desc = "Undo last stage" },
-    ['<leader>gtb'] = { cmd = '<cmd>Gitsigns toggle_current_line_blame<CR>', desc = "Toggle Blamer" },
-    ['<leader>gts'] = { cmd = '<cmd>Gitsigns toggle_signs<CR>',              desc = "Toggle Sings" },
-    ['<leader>gtn'] = { cmd = '<cmd>Gitsigns toggle_numhl<CR>',              desc = "Toggle Number highlight" },
-    ['<leader>gtl'] = { cmd = '<cmd>Gitsigns toggle_linehl<CR>',             desc = "Toggle Line highlight" },
-    ['<leader>gtw'] = { cmd = '<cmd>Gitsigns toggle_word_diff<CR>',          desc = "Toggle Work_Diff" },
-    ['<leader>gtd'] = { cmd = '<cmd>Gitsigns toggle_deleted<CR>',            desc = "Toggle Deleted Line" },
+    ['<leader>gk']  = { cmd = '<cmd>Gitsigns prev_hunk<CR>',                 desc = 'Jump to previous hunk' },
+    ['<leader>gj']  = { cmd = '<cmd>Gitsigns next_hunk<CR>',                 desc = 'Jump to next hunk' },
+    ['<leader>gb']  = { cmd = '<cmd>Gitsigns blame_line<CR>',                desc = 'Show Blamer' },
+    ['<leader>gv']  = { cmd = '<cmd>Gitsigns preview_hunk<CR>',              desc = 'Preview hunk' },
+    ['<leader>gr']  = { cmd = '<cmd>Gitsigns reset_hunk<CR>',                desc = 'Reset hunk' },
+    ['<leader>gR']  = { cmd = '<cmd>Gitsigns reset_buffer<CR>',              desc = 'Reset all hunk' },
+    ['<leader>gs']  = { cmd = '<cmd>Gitsigns stage_hunk<CR>',                desc = 'Stage hunk' },
+    ['<leader>gu']  = { cmd = '<cmd>Gitsigns undo_stage_hunk<CR>',           desc = 'Undo last stage' },
+    ['<leader>gtb'] = { cmd = '<cmd>Gitsigns toggle_current_line_blame<CR>', desc = 'Toggle Blamer' },
+    ['<leader>gts'] = { cmd = '<cmd>Gitsigns toggle_signs<CR>',              desc = 'Toggle Sings' },
+    ['<leader>gtn'] = { cmd = '<cmd>Gitsigns toggle_numhl<CR>',              desc = 'Toggle Number highlight' },
+    ['<leader>gtl'] = { cmd = '<cmd>Gitsigns toggle_linehl<CR>',             desc = 'Toggle Line highlight' },
+    ['<leader>gtw'] = { cmd = '<cmd>Gitsigns toggle_word_diff<CR>',          desc = 'Toggle Work_Diff' },
+    ['<leader>gtd'] = { cmd = '<cmd>Gitsigns toggle_deleted<CR>',            desc = 'Toggle Deleted Line' },
   },
 }
 
 M.Trouble = {
   n = {
-    ['<leader>xx'] = { cmd = '<cmd>TroubleToggle<CR>',                                  desc = "Trouble" },
-    ['<leader>xw'] = { cmd = '<cmd>TroubleToggle workspace_diagnostics<CR>',            desc = 'Workspace Diagnostics in Trouble'},
-    ['<leader>xd'] = { cmd = '<cmd>TroubleToggle document_diagnostics<CR>',             desc = 'Document Diagnostics in Trouble' },
-    ['<leader>xq'] = { cmd = '<cmd>TroubleToggle quickfix<CR>',                         desc = 'Quickfix in Trouble' },
-    ['<leader>xl'] = { cmd = '<cmd>TroubleToggle loclist<CR>',                          desc = 'Loclist in Trouble' },
-    ['<leader>xk'] = { cmd = '<cmd>lua require("trouble").previous({skip_groups = true, jump = true})<CR>', desc = "Previous in Trouble" },
-    ['<leader>xj'] = { cmd = '<cmd>lua require("trouble").next({skip_groups = true, jump = true})<CR>', desc = 'Next in Trouble' },
+    ['<leader>xx'] = { cmd = '<cmd>TroubleToggle<CR>',                                                     desc = 'Trouble' },
+    ['<leader>xw'] = { cmd = '<cmd>TroubleToggle workspace_diagnostics<CR>',                               desc = 'Workspace Diagnostics in Trouble', },
+    ['<leader>xd'] = { cmd = '<cmd>TroubleToggle document_diagnostics<CR>',                                desc = 'Document Diagnostics in Trouble', },
+    ['<leader>xq'] = { cmd = '<cmd>TroubleToggle quickfix<CR>',                                            desc = 'Quickfix in Trouble' },
+    ['<leader>xl'] = { cmd = '<cmd>TroubleToggle loclist<CR>',                                             desc = 'Loclist in Trouble' },
+    ['<leader>xk'] = { cmd = '<cmd>lua require("trouble").previous({skip_groups = true,jump = true})<CR>', desc = 'Previous in Trouble', },
+    ['<leader>xj'] = { cmd = '<cmd>lua require("trouble").next({skip_groups = true,jump = true})<CR>',     desc = 'Next in Trouble', },
   },
 }
 
 M.TodoComments = {
   n = {
-    ['<leader>xt'] = { cmd = '<cmd>TroubleToggle todo<CR>', desc = "Todo-Comments" },
-    ['<leader>xT'] = { cmd = '<cmd>TroubleToggle todo keywords=TODO,FIX disable_not_found_warnings=true<CR>', desc = "Todo-Comments { TODO | FIX }" },
+    ['<leader>xt'] = { cmd = '<cmd>TroubleToggle todo<CR>',                                                   desc = 'Todo-Comments' },
+    ['<leader>xT'] = { cmd = '<cmd>TroubleToggle todo keywords=TODO,FIX disable_not_found_warnings=true<CR>', desc = 'Todo-Comments [TODO|FIX]' },
   },
 }
 
 M.Bufferline = {
   n = {
-    ['bk'] = '<cmd>BufferLineCyclePrev<CR>',
-    ['bj'] = '<cmd>BufferLineCycleNext<CR>',
-    ['bK'] = '<cmd>BufferLineMovePrev<CR>',
-    ['bJ'] = '<cmd>BufferLineMoveNext<CR>',
-    ['bp'] = '<cmd>BufferLinePick<CR>',
-    ['bP'] = '<cmd>BufferLineTogglePin<CR>',
-    ['bq'] = '<cmd>bdelete!<CR>',
+    ['bk'] = { cmd = '<cmd>BufferLineCyclePrev<CR>', desc = 'previous buffer'},
+    ['bj'] = { cmd = '<cmd>BufferLineCycleNext<CR>', desc = 'next buffer'},
+    ['bK'] = { cmd = '<cmd>BufferLineMovePrev<CR>',  desc = 'Move buffer forward' },
+    ['bJ'] = { cmd = '<cmd>BufferLineMoveNext<CR>',  desc = 'Move buffer backward' },
+    ['bp'] = { cmd = '<cmd>BufferLinePick<CR>',      desc = 'pick a buffer' },
+    ['bP'] = { cmd = '<cmd>BufferLineTogglePin<CR>', desc = 'pin a buffer' },
+    ['bq'] = { cmd = '<cmd>bdelete!<CR>',            desc = 'close buffer'},
   },
 }
 
 M.Diffview = {
   n = {
-    ['<leader>gd'] = '<cmd>DiffviewOpen<CR>',
+    ['<leader>gd'] = { cmd = '<cmd>DiffviewOpen<CR>', desc = 'Git Diffview'},
   },
 }
 
@@ -178,21 +178,21 @@ M.ToggleTerm = {
     ['<leader>lr'] = { cmd = "<cmd>lua require('launcher').toolToggle('ranger')<CR>", desc = 'Ranger' },
     ['<leader>ld'] = { cmd = "<cmd>lua require('launcher').toolToggle('docker')<CR>", desc = 'LazyDocker' },
     ['<leader>lg'] = { cmd = "<cmd>lua require('launcher').toolToggle('git')<CR>",    desc = 'LazyGit' },
-    ['<C-b>n'] = { cmd = "<cmd>lua require('launcher').term_new()<CR>",               desc = 'New Terminal' },
-    ['<C-b>k'] = { cmd = "<cmd>lua require('launcher').term_prev()<CR>",              desc = 'Previous Terminal' },
-    ['<C-b>j'] = { cmd = "<cmd>lua require('launcher').term_next()<CR>",              desc = 'Next Terminal' },
+    ['<C-b>n']     = { cmd = "<cmd>lua require('launcher').term_new()<CR>",           desc = 'New Terminal' },
+    ['<C-b>k']     = { cmd = "<cmd>lua require('launcher').term_prev()<CR>",          desc = 'Previous Terminal' },
+    ['<C-b>j']     = { cmd = "<cmd>lua require('launcher').term_next()<CR>",          desc = 'Next Terminal' },
   },
   t = {
-    ['<C-b>n'] = "<cmd>lua require('launcher').term_new()<CR>",
-    ['<C-b>k'] = "<cmd>lua require('launcher').term_prev()<CR>",
-    ['<C-b>j'] = "<cmd>lua require('launcher').term_next()<CR>",
+    ['<C-b>n'] = { cmd = "<cmd>lua require('launcher').term_new()<CR>",  desc = 'new terminal' },
+    ['<C-b>k'] = { cmd = "<cmd>lua require('launcher').term_prev()<CR>", desc = 'previous terminal'},
+    ['<C-b>j'] = { cmd = "<cmd>lua require('launcher').term_next()<CR>", desc = 'next terminal'},
   },
 }
 
 M.DB = {
   n = {
-    ['<leader>zz'] = { cmd = "<cmd>DBUIToggle<CR>", desc = 'Database Manager'}
-  }
+    ['<leader>zz'] = { cmd = '<cmd>DBUIToggle<CR>', desc = 'Database Manager' },
+  },
 }
 
 -- lsp keybind
