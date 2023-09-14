e90265f4f55b6f951c99c066e9013fc40599fbf3
BYT0723 <twang9739@gmail.com>
2023-09-14 22:17:36 +0800

    fix: spectre keymap in visual mode

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -88,10 +88,10 @@ M.Telescope = {
 
 -- stylua: ignore
 M.Spectre = {
-  {'<leader>S',  function() require("spectre").toggle() end,                             mode = 'n', desc = "Toggle Spectre"},
-  {'<leader>sw', function() require("spectre").open_visual({select_word=true}) end,      mode = 'n', desc = "Search current word"},
-  {'<leader>sw', function() require("spectre").open_visual() end,                        mode = 'v', desc = "Search current word"},
-  {'<leader>sp', function() require("spectre").open_file_search({select_word=true}) end, mode = 'n', desc = "Search on current file"}
+  {'<leader>S' , function() require('spectre').toggle() end                                , mode = 'n', desc = 'Toggle Spectre'        ,},
+  {'<leader>sw', function() require('spectre').open_visual({ select_word = true }) end     , mode = 'n', desc = 'Search current word'   ,},
+  {'<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>'                      , mode = 'v', desc = 'Search current word'   ,},
+  {'<leader>sp', function() require('spectre').open_file_search({ select_word = true }) end, mode = 'n', desc = 'Search on current file',},
 }
 
 M.DB = {
