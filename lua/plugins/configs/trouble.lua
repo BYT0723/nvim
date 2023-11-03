b2e000410a1d888481f6a6316cc76d5c10c06592
BYT0723 <twang9739@gmail.com>
2023-11-03 14:55:09 +0800

    perf: trouble configuration

--- lua/plugins/configs/trouble.lua
+++ lua/plugins/configs/trouble.lua
@@ -7,7 +7,7 @@ local options = {
   fold_open = '', -- icon used for open folds
   fold_closed = '', -- icon used for closed folds
   group = true, -- group results by file
-  padding = true, -- add an extra new line on top of the list
+  padding = false, -- add an extra new line on top of the list
   action_keys = { -- key mappings for actions in the trouble list
     close = 'q', -- close the list
     cancel = '<esc>', -- cancel the preview and get back to your last window / buffer / cursor
@@ -27,13 +27,13 @@ local options = {
     previous = 'k', -- preview item
     next = 'j', -- next item
   },
-  indent_lines = false, -- add an indent guide below the fold icons
+  indent_lines = true, -- add an indent guide below the fold icons
   auto_open = false, -- automatically open the list when you have diagnostics
   auto_close = false, -- automatically close the list when you have no diagnostics
   auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
   auto_fold = false, -- automatically fold a file trouble list at creation
   auto_jump = { 'lsp_definitions' }, -- for the given modes, automatically jump if there is only a single result
-  use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+  use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
 }
 
 return options
