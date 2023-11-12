070b7b9c526a8bab45203af6cf5f7ad4c0f9abdb
BYT0723 <twang9739@gmail.com>
2023-11-12 22:33:14 +0800

    fix: tabnine keymapping conflict with nvim-cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -30,8 +30,8 @@ require('lazy').setup({
     config = function()
       require('tabnine').setup({
         disable_auto_comment = true,
-        accept_keymap = '<Tab>',
-        dismiss_keymap = '<C-]>',
+        accept_keymap = '<C-l>',
+        dismiss_keymap = '<C-h>',
         debounce_ms = 800,
         suggestion_color = { gui = '#808080', cterm = 244 },
         exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
