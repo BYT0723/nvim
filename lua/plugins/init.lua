d533f26b42119cd4521efc26e956b1392a30ddef
BYT0723 <twang9739@gmail.com>
2024-02-29 10:37:30 +0800

    perf: project.nvim configuratoin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -259,36 +259,26 @@ require('lazy').setup({
     'ahmedkhalf/project.nvim',
     config = function()
       require('project_nvim').setup({
-        -- Manual mode doesn't automatically change your root directory, so you have
-        -- the option to manually do so using `:ProjectRoot` command.
         manual_mode = false,
         detection_methods = { 'lsp', 'pattern' },
-        patterns = { '.git', '_darcs', '.hg', '.bzr', '.svn', 'Makefile', 'package.json', 'go.mod' },
-
-        -- Table of lsp clients to ignore by name
-        -- eg: { "efm", ... }
-        ignore_lsp = {},
-
-        -- Don't calculate root dir on specific directories
-        -- Ex: { "~/.cargo/*", ... }
+        patterns = { '.git', 'Makefile', 'package.json', 'go.mod' },
+        ignore_lsp = {
+          'bashls',
+          'cssls',
+          'dockerls',
+          'docker_compose_language_service',
+          'dotls',
+          'emmet_ls',
+          'jsonls',
+          'marksman',
+          'pyright',
+          'rust_analyzer',
+          'tsserver',
+        },
         exclude_dirs = {},
-
-        -- Show hidden files in telescope
         show_hidden = false,
-
-        -- When set to false, you will get a message when project.nvim changes your
-        -- directory.
         silent_chdir = true,
-
-        -- What scope to change the directory, valid options are
-        -- * global (default)
-        -- * tab
-        -- * win
         scope_chdir = 'global',
-
-        -- Path where project.nvim will store the project history for use in
-        -- telescope
-        datapath = vim.fn.stdpath('data'),
       })
     end,
   },
