ffaca345c093894546db7b271f7566f057fae487
BYT0723 <twang9739@gmail.com>
2024-05-14 18:27:18 +0800

    perf: zen-mode config

--- lua/plugins/configs/zen-mode.lua
+++ lua/plugins/configs/zen-mode.lua
@@ -31,7 +31,7 @@ return {
       laststatus = 0, -- turn off the statusline in zen mode
     },
     twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-    gitsigns = { enabled = false }, -- disables git signs
+    gitsigns = { enabled = true }, -- disables git signs
     tmux = { enabled = false }, -- disables the tmux statusline
     -- this will change the font size on kitty when in zen mode
     -- to make this work, you need to set the following kitty options:
