f623fd14a0135412679467ae7478b7576a8f63da
BYT0723 <twang9739@gmail.com>
2024-06-10 23:59:53 +0800

    fix: trouble.nvim configuration

--- lua/plugins/configs/trouble.lua
+++ lua/plugins/configs/trouble.lua
@@ -2,7 +2,6 @@ return {
   position = 'bottom', -- position of the list can be: bottom, top, left, right
   height = 10, -- height of the trouble list when position is top or bottom
   width = 50, -- width of the list when position is left or right
-  icons = true, -- use devicons for filenames
   mode = 'workspace_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
   fold_open = '', -- icon used for open folds
   fold_closed = '', -- icon used for closed folds
