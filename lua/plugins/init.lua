63abf1ee23247ef726d5bd3af95ea5ba227a5689
BYT0723 <twang9739@gmail.com>
2024-07-12 09:49:56 +0800

    theme(telescope): change telescope highlight

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -56,6 +56,37 @@ require('lazy').setup({
         floats = 'transparent', -- style for floating windows
       },
       sidebars = { 'qf' },
+      on_colors = function(colors) end,
+      on_highlights = function(hl, c)
+        local prompt = '#2d3149'
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = c.hint,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
     },
   },
   -- background transparent
