22af0e836d00c074441f2a35a5e2e1e198787961
BYT0723 <twang9739@gmail.com>
2024-07-12 09:50:16 +0800

    perf(neovide): remove floating shadow

--- lua/base/environment.lua
+++ lua/base/environment.lua
@@ -40,6 +40,7 @@ if vim.g.neovide then
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_refresh_rate_idle = 20
   vim.g.neovide_transparency = 0.9
+  vim.g.neovide_floating_shadow = false
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_trail_size = 0.5
