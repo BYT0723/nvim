61015df8ac84c37f8e3e37e40ce08571817b097c
BYT0723 <twang9739@gmail.com>
2024-07-15 22:02:18 +0800

    fix(which-key): fix configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -538,11 +538,8 @@ require('lazy').setup({
     'folke/which-key.nvim',
     enabled = true,
     opts = {
-      window = {
+      win = {
         border = 'none', -- none, single, double, shadow
-        position = 'top', -- bottom, top
-        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
       },
       layout = {
         align = 'center',
