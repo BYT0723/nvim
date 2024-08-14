c24947c6989ae51492a8f1ed68b3f90e6aab1ff5
BYT0723 <twang9739@gmail.com>
2024-08-14 17:22:43 +0800

    chore: neovide configuration

--- lua/base/environment.lua
+++ lua/base/environment.lua
@@ -52,14 +52,14 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.3 -- s 秒
   vim.g.neovide_scroll_animation_far_lines = 1
   vim.g.neovide_no_idle = true
-  vim.g.neovide_confirm_quit = false
+  vim.g.neovide_confirm_quit = true
   vim.g.neovide_fullscreen = true
   vim.g.neovide_profiler = false -- debug information
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = true
   vim.g.neovide_cursor_vfx_mode = 'railgun' -- 粒子效果: wireframe | ripple | railgun | torpedo | pixiedust | sonicboom
   set.guifont = 'CaskaydiaCove Nerd Font:h9.5'
-  set.linespace = 2
+  set.linespace = 1
 end
 
 -- vim-go configuration
