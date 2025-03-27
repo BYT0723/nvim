5834188050117bedb473489d3f97d9bc19b45c5e
BYT0723 <twang9739@gmail.com>
2025-03-27 13:37:33 +0800

    perf: gitsigns

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,37 +1,30 @@
 return {
-  signs = {
-    add = { text = '▊' },
-    change = { text = '▊' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked = { text = '┆' },
-  },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
     follow_files = true,
   },
   auto_attach = true,
-  attach_to_untracked = true,
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  attach_to_untracked = false,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
     virt_text_priority = 100,
+    use_focus = true,
   },
   current_line_blame_formatter = '  「 <author>, <author_time> • <summary> 」',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
-  max_file_length = 40000,
+  max_file_length = 40000, -- Disable if file is longer than this (in lines)
   preview_config = {
     -- Options passed to nvim_open_win
-    border = 'none',
+    border = 'rounded',
     style = 'minimal',
     relative = 'cursor',
     row = 0,
@@ -69,7 +62,6 @@ return {
     map('n', '<leader>gtd', gitsigns.toggle_deleted,                              { desc = 'Toggle Deleted Line' })
 
     -- Text object
-    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end,
 }
