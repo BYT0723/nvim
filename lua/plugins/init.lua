4eaa0e72d01acd6f5b9422dc38f2f769c6c452af
BYT0723 <twang9739@gmail.com>
2025-04-15 21:25:04 +0800

    perf: dashboard

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,7 +41,15 @@ require('lazy').setup({
     ---@type snacks.Config
     opts = {
       bigfile = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = {
+        sections = {
+          { section = 'header' },
+          { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+      },
       explorer = { enabled = false },
       indent = { indent = { char = '╎' }, scope = { char = '╎' } },
       input = { enabled = true, icon_pos = 'title', win = { width = 40, relative = 'cursor', row = -3, col = 0 } },
