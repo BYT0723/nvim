86c012c48db1a4878c597a8d17ae7a214d85e2b9
BYT0723 <twang9739@gmail.com>
2025-07-15 18:00:19 +0800

    perf: snacker dashboard header

--- lua/plugins/configs/snacks.lua
+++ lua/plugins/configs/snacks.lua
@@ -2,12 +2,17 @@
 return {
   bigfile = { enabled = true },
   dashboard = {
-    sections = {
-      { section = 'header' },
-      { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
-      { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-      { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-      { section = 'startup' },
+    preset = {
+      header = [[
+███╗   ██╗ ███████╗  ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
+████╗  ██║ ██╔════╝ ██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
+██╔██╗ ██║ █████╗   ██║   ██║ ██║   ██║ ██║ ██╔████╔██║
+██║╚██╗██║ ██╔══╝   ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║
+██║ ╚████║ ███████╗ ╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║
+╚═╝  ╚═══╝ ╚══════╝  ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
+
+Never alone, never apart.
+]],
     },
   },
   explorer = { enabled = false },
